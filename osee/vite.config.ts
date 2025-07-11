6cdb1355fd2541f722bd1969a17fda127007bd34
Hatef <h.kouzechi@gmail.com>
2025-07-11 17:26:01 +0400

    maybe fixed - claude 17

--- osee/vite.config.ts
+++ osee/vite.config.ts
@@ -1,11 +1,9 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      plugins: [react()],
       base: '/public/osee/',
       build: {
         outDir: './build/',
